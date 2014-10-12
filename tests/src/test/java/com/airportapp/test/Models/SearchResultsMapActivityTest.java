a9dc05e8532f5e00494741ad48a79a04e6bf155d
chinangela <achin@hmc.edu>
2014-10-11 20:30:01 -0700

    Wrote all tests for switching between activities

--- tests/src/test/java/com/airportapp/test/Models/SearchResultsMapActivityTest.java
+++ tests/src/test/java/com/airportapp/test/Models/SearchResultsMapActivityTest.java
@@ -1,6 +1,7 @@
 package com.airportapp.test.Models;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -8,15 +9,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 import org.robolectric.Robolectric;
+import org.robolectric.util.ActivityController;
+
+import com.airportapp.R;
+import com.airportapp.SearchResultsListActivity;
 import com.airportapp.test.MyRobolectricTestRunner;
 import com.airportapp.SearchResultsMapActivity;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.Robolectric.buildActivity;
+import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(MyRobolectricTestRunner.class)
 
 public class SearchResultsMapActivityTest {
+
+    private final ActivityController<SearchResultsMapActivity> controller = buildActivity(SearchResultsMapActivity.class);
+
+
     @Before
     public void setup() {
         //do whatever is necessary before every test
@@ -24,8 +37,18 @@ public class SearchResultsMapActivityTest {
 
     @Test
     public void testActivityFound() {
-        Activity activity = Robolectric.buildActivity(SearchResultsMapActivity.class).create().get();
+        Activity activity = buildActivity(SearchResultsMapActivity.class).create().get();
 
         Assert.assertNotNull(activity);
+    }
+
+    // Tests that the list button will send the user to a list view of search results.
+    @Test
+    public void test_clickingSearchResultsList_shouldStartSearchResultsListActivity() {
+        SearchResultsMapActivity activity = controller.create().start().resume().get();
+        activity.findViewById(R.id.searchResultsMap_button).performClick();
+
+        Intent expectedIntent = new Intent(activity, SearchResultsListActivity.class);
+        assertThat(shadowOf(activity).getNextStartedActivity(), equalTo(expectedIntent));
     }
 }
