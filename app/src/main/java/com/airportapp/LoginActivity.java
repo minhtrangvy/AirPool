685e0f5c4d64527d54f1716569ef8e6ed678ce4e
Maury Quijada <maury.quijada@gmail.com>
2014-10-11 22:09:00 -0700

    Add Facebook SDK and try to implement login. At this commit, tests are not working.

--- app/src/main/java/com/airportapp/LoginActivity.java
+++ app/src/main/java/com/airportapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.airportapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,27 +11,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 
-public class LoginActivity extends Activity implements View.OnClickListener {
 
-    EditText editUsername;
-    EditText editPassword;
-    Button mainButton;
+public class LoginActivity extends Activity {
+    private static final String TAG = "MainFragment";
+
+    private Session.StatusCallback loginCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            Log.i("LoginActivity", "HAI");
+            onSessionStateChange(session, state, exception);
+        }
+    };
+    private UiLifecycleHelper uiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        // Access the Button defined in login XML
-        // and listen for it here
-        mainButton = (Button) findViewById(R.id.main_button);
-        mainButton.setOnClickListener(this);
-
-        editUsername = (EditText) findViewById(R.id.username_edittext);
-        editPassword = (EditText) findViewById(R.id.password_edittext);
+        uiHelper = new UiLifecycleHelper(this, loginCallback);
+        uiHelper.onCreate(savedInstanceState);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,9 +58,53 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent clickLogin = new Intent(LoginActivity.this, PreferencesActivity.class);
-        startActivity(clickLogin);
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (session.isOpened()) {
+            Log.i(TAG, "User has logged in...");
+            // make request to the /me API
+            Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                // callback after Graph API response with user object
+                @Override
+                public void onCompleted(GraphUser user, Response response) {
+                    if (user != null) {
+                        Log.i(TAG, "Hello " + user.getId());
+                    }
+                }
+            }).executeAsync();
+        } else if (session.isClosed()) {
+            Log.i(TAG, "User has logged out...");
+        }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
 }
