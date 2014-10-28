8eca2e8487a32f7d41fdcc9fb12743e0c3a35575
chinangela <achin@hmc.edu>
2014-10-27 21:35:56 -0700

    Added spinners and calendar to select departure date/time, as well as other small UI changes

--- app/src/main/java/com/airportapp/ViewGroupActivity.java
+++ app/src/main/java/com/airportapp/ViewGroupActivity.java
@@ -7,21 +7,37 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ViewGroupActivity extends Activity implements View.OnClickListener {
 
-    Button backButton;
+    Button backButton, joinButton, editButton;
+    boolean userNotMember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_group);
 
+        userNotMember = true;
+
+        // TODO: Update once Parse is working
+        if (userNotMember) {
+            View editButton = findViewById(R.id.editGroup_button);
+            editButton.setVisibility(View.GONE);
+        }
+
         // Access the Button defined in login XML
         // and listen for it here
         backButton = (Button) findViewById(R.id.searchResultsList_button);
         backButton.setOnClickListener(this);
+
+        joinButton = (Button) findViewById(R.id.joinGroup_button);
+        joinButton.setOnClickListener(this);
+
+        editButton = (Button) findViewById(R.id.editGroup_button);
+        editButton.setOnClickListener(this);
     }
 
 
@@ -46,7 +62,23 @@ public class ViewGroupActivity extends Activity implements View.OnClickListener 
 
     @Override
     public void onClick(View view) {
-        Intent clickLogin = new Intent(ViewGroupActivity.this, SearchResultsListActivity.class);
-        startActivity(clickLogin);
+        switch (view.getId()) {
+            case R.id.searchResultsList_button:
+                Intent clickLogin = new Intent(ViewGroupActivity.this, SearchResultsListActivity.class);
+                startActivity(clickLogin);
+                break;
+            case R.id.joinGroup_button:
+                // TODO: Store that user has joined group
+                editButton.setVisibility(View.VISIBLE);
+                Toast.makeText(getApplicationContext(),
+                        "Successfully Joined Group!",
+                        Toast.LENGTH_SHORT).show();
+                userNotMember = false;
+                break;
+            case R.id.editGroup_button:
+                Intent clickEdit = new Intent(ViewGroupActivity.this, EditGroupActivity.class);
+                startActivity(clickEdit);
+                break;
+        }
     }
 }
