99820f0e3823c2eaccc31256d6303121e0b8491b
chinangela <achin@hmc.edu>
2014-10-31 20:25:04 -0700

    Subclassed the Group Object in Parse

--- app/src/main/java/com/airportapp/CreateGroupActivity.java
+++ app/src/main/java/com/airportapp/CreateGroupActivity.java
@@ -1,9 +1,17 @@
 package com.airportapp;
 
 import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.parse.Parse;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import org.json.JSONArray;
 //import com.parse.Parse;
 //import com.parse.ParseAnalytics;
 
@@ -12,8 +20,10 @@ public class CreateGroupActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        Parse.initialize(this, "JFLuGOh9LQsqGsbVwuunD9uSSXgp8hDuDGBgHguJ", "0x2FoxHDKmIF81PqcK0wuh8OS8Ga2FsM6RTUmmcu");
+        ParseObject.registerSubclass(Group.class);
+        Parse.initialize(this, "JFLuGOh9LQsqGsbVwuunD9uSSXgp8hDuDGBgHguJ", "0x2FoxHDKmIF81PqcK0wuh8OS8Ga2FsM6RTUmmcu");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_create_group);
     }
 
@@ -36,4 +46,88 @@ public class CreateGroupActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    @ParseClassName("Group")
+    public class Group extends ParseObject {
+
+        public Group() {
+            // A default constructor is required.
+        }
+
+        public String getGroupID() {
+            return getString("groupID");
+        }
+
+        public void setGroupID(String groupID) {
+            put("groupID", groupID);
+        }
+
+        public String getDate() {
+            return getString("date");
+        }
+
+        public void setDate(String date) {
+            put("date", date);
+        }
+
+        public String getTime() {
+            return getString("time");
+        }
+
+        public void setTime(String time) {
+            put("time", time);
+        }
+
+        public String getTransPref() {
+            return getString("transPref");
+        }
+
+        public void setTransPref(String transPref) {
+            put("transPref", transPref);
+        }
+
+        public String getAirport() {
+            return getString("airport");
+        }
+
+        public void setAirport(String airport) {
+            put("airport", airport);
+        }
+
+        public String getCollege() {
+            return getString("college");
+        }
+
+        public void setCollege(String college) {
+            put("college", college);
+        }
+
+        public boolean getToFrom() {
+            return getBoolean("toFrom");
+        }
+
+        public void setToFrom(String toFrom) {
+            put("toFrom", toFrom);
+        }
+
+        public JSONArray getMembers() {
+            return getJSONArray("members");
+        }
+
+        public void setMembers(JSONArray members) {
+            put("members", members);
+        }
+
+        public boolean getGroupOpen() {
+            return getBoolean("groupOpen");
+        }
+
+        public void setGroupOpen(String groupOpen) {
+            put("groupOpen", groupOpen);
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/airportapp/EditGroupActivity.java
+++ app/src/main/java/com/airportapp/EditGroupActivity.java
@@ -5,10 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import org.json.JSONArray;
+
 public class EditGroupActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ParseObject.registerSubclass(Group.class);
+        Parse.initialize(this, "JFLuGOh9LQsqGsbVwuunD9uSSXgp8hDuDGBgHguJ", "0x2FoxHDKmIF81PqcK0wuh8OS8Ga2FsM6RTUmmcu");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_group);
     }
@@ -31,5 +40,86 @@ public class EditGroupActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @ParseClassName("Group")
+    public class Group extends ParseObject {
+
+        public Group() {
+            // A default constructor is required.
+        }
+
+        public String getGroupID() {
+            return getString("groupID");
+        }
+
+        public void setGroupID(String groupID) {
+            put("groupID", groupID);
+        }
+
+        public String getDate() {
+            return getString("date");
+        }
+
+        public void setDate(String date) {
+            put("date", date);
+        }
+
+        public String getTime() {
+            return getString("time");
+        }
+
+        public void setTime(String time) {
+            put("time", time);
+        }
+
+        public String getTransPref() {
+            return getString("transPref");
+        }
+
+        public void setTransPref(String transPref) {
+            put("transPref", transPref);
+        }
+
+        public String getAirport() {
+            return getString("airport");
+        }
+
+        public void setAirport(String airport) {
+            put("airport", airport);
+        }
+
+        public String getCollege() {
+            return getString("college");
+        }
+
+        public void setCollege(String college) {
+            put("college", college);
+        }
+
+        public boolean getToFrom() {
+            return getBoolean("toFrom");
+        }
+
+        public void setToFrom(String toFrom) {
+            put("toFrom", toFrom);
+        }
+
+        public JSONArray getMembers() {
+            return getJSONArray("members");
+        }
+
+        public void setMembers(JSONArray members) {
+            put("members", members);
+        }
+
+        public boolean getGroupOpen() {
+            return getBoolean("groupOpen");
+        }
+
+        public void setGroupOpen(String groupOpen) {
+            put("groupOpen", groupOpen);
+        }
+
     }
 }
